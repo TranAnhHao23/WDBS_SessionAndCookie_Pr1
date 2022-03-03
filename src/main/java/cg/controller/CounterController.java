2e298698f42110e6b2b36476c4e9215c315c666f
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-03-03 11:12:16 +0700

    add file

---
+++ src/main/java/cg/controller/CounterController.java
@@ -0,0 +1,25 @@
+package cg.controller;
+
+import cg.model.Counter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@SessionAttributes("counter")
+public class CounterController {
+
+    @ModelAttribute("counter")
+    public Counter setupCounter(){
+        return new Counter();
+    }
+
+    @GetMapping("/")
+    public ModelAndView get(@ModelAttribute("counter") Counter counter){
+        ModelAndView modelAndView = new ModelAndView("index");
+        counter.increment();
+        return modelAndView;
+    }
+}
